5ce085ac409b023306df9c19bbd8fab718785b8d
dahyeon-j <snoopy809622@gmail.com>
2021-03-18 13:59:07 +0900

    implement tinycListenerPrinter.py

---
+++ gen/tinycListenerPrinter.py
@@ -0,0 +1,26 @@
+import sys
+from antlr4 import *
+from antlr4.tree.Tree import Tree # make lisp stype tree
+import tinycParser
+import tinycLexer
+import tinycListener
+
+def main(file_name):
+    input = FileStream(file_name)
+    lexer = tinycLexer.tinycLexer(input)
+
+    stream = CommonTokenStream(lexer)
+
+    parser = tinycParser.tinycParser(stream)
+
+    tree = parser.program()
+
+    printer = tinycListener.tinycListener()
+
+    walker = ParseTreeWalker()
+
+    walker.walk(printer, tree)
+
+if __name__ == '__main__':
+    main("input.c")
+
